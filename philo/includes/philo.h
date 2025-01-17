327835684309bd962a0c6a5852dfd308b6a25623
Yi-Chun LIN <105208178+ychun816@users.noreply.github.com>
2025-01-17 19:11:58 +0000

    finish init_table, mutex, philo

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -29,6 +29,8 @@
 # define SUCCESS 0
 # define FAILURE 1
 
+# define STDERR 2
+
 
 /***** STRUCTURES *****/
 
@@ -51,32 +53,61 @@ typedef enum e_state
 //     pthread_mutex_t mutex;          // Mutex for the fork
 // }   t_fork;
 
+typedef struct s_philo  t_philo;
+typedef struct s_table  t_table;
+
 typedef struct s_philo 
 {
     int             id;
     int             eat_count;
     unsigned long   last_meal_time;
-    pthread_t       thread;
-    pthread_mutex_t meal_mutex;
-    struct s_table  *table;
+    t_table  *ph_table;
+    // pthread_t       thread;
     pthread_mutex_t *left_fork;
     pthread_mutex_t *right_fork;
+    pthread_mutex_t meal_mutex;
 } t_philo;
 
 typedef struct s_table 
 {
-    int philo_count;
+    int nb_philo;
     unsigned long   time_to_die;
     unsigned long   time_to_eat;
     unsigned long   time_to_sleep;
-    int must_eat_count;
-    bool    meal_end;
+    int nb_must_eat;
+    bool    feast_should_stop;//used as flag: true-> stimulation should stop
     unsigned long   start_time;
+    t_philo *philos;
     pthread_mutex_t *forks;
     pthread_mutex_t print_mutex;
     pthread_mutex_t death_mutex;
-    t_philo *philo;
 } t_table;
+
+/* OG thoughts on s_forks stuct? 
+struct s_forks
+{
+    pthread_mutex_t *forks;
+    pthread_mutex_t print_mutex;
+    pthread_mutex_t death_mutex;
+};
+If you use this structure, 
+each philosopher would need access to the s_forks instance to get their respective fork mutexes. Here’s why this can be more complex:
+
+a. Increased indirection
+You would need to pass the s_forks structure to every function or thread that requires access to the forks, print_mutex, or death_mutex.
+This adds unnecessary complexity, as the table structure (t_table) already centralizes this information.
+b. Redundant encapsulation
+Mutexes for forks are naturally grouped in t_table. Adding another structure (s_forks) to encapsulate the same data results in redundant abstraction.
+c. Complicates indexing
+Instead of forks[i], 
+you’d need to write forks->forks[i]. 
+This extra layer adds verbosity and reduces clarity.
+d. No clear benefit
+The s_forks structure doesn’t add any functional advantage. 
+The same goal (managing mutexes for forks) is already achieved with table->forks.
+
+*/
+
 
 
 /*********************/
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -28,20 +28,19 @@ int main(int ac, char *av[])
     i = 0;
 
     if (ac < 5 || ac > 6)
-        return (ft_putstr_fd("Invalid Arguments\n"), FAILURE);
-    ft_memset(&table, 0, sizeof(t_table));
+        return (ft_putstr_fd("Invalid Arguments\n", STDERR), FAILURE);
+    // ft_memset(&table, 0, sizeof(t_table));
     // if (parse_n_check_args(ac, av, table))
     //     ft_putstr_fd("Invalid Arguments\n");
     if (init_table(ac, av, &table))
-        return (ft_putstr_fd("Initialization Failed\n"), FAILURE);
-    //start table
+        return (ft_putstr_fd("Initialization Failed\n", STDERR), FAILURE);
     if (start_feast(&table))
     {
-        ft_putstr_fd("Similation Failed\n");
-        cleanup_table(&table);
+        ft_putstr_fd("Similation Failed! Can't Eat :(\n", STDERR);
+        cleanup_all(&table);
         return (FAILURE);   
     }
-    cleanup_table(&table);
+    cleanup_all(&table);
     return (SUCCESS);
 }
 
@@ -52,15 +51,142 @@ int main(int ac, char *av[])
 // {
 // }
 
-/** init table */
+/** init table 
+ * @note 
+ * table->must_eat_count = -1;
+ * -> "-1" means there is no "must eat" condition (unlimited eating until the simulation ends due to other factors, such as a philosopher's death).
+ * table->feast_should_stop = false;
+ * -> It acts as a flag to indicate whether the simulation has concluded. 
+ * -> The value starts as false because the simulation hasn't started yet and will remain false while philosophers are eating, thinking, and living.
+ * -> It signals that the simulation should stop, likely because one of the philosophers has died (or another termination condition has been met).
+ * 
+ * 
+*/
 int init_table(int ac, char *av[], t_table *table)
 {
-    
+    ft_memset(&table, 0, sizeof(t_table)); // Zero-initialize the structure
+
+    table->nb_philo = ft_atoi(av[1]);
+    table->time_to_die = ft_atoi(av[2]);
+    table->time_to_eat = ft_atoi(av[3]);
+    table->time_to_sleep = ft_atoi(av[4]);
+    if (ac == 6)
+        table->nb_must_eat = ft_atoi(av[5]);
+    else
+        table->nb_must_eat = -1;
+    if (table->nb_philo <= 1 || table->time_to_die < 0 ||
+        table->time_to_eat < 0 || table->time_to_sleep < 0)
+        return (FAILURE);
+    table->feast_should_stop = false;
+    if (init_mutex(table))
+        return (FAILURE);
+    if (init_philo(table))
+        return (FAILURE);
+    return (SUCCESS);
 }
 
+/** INIT MUTEX
+ * @note
+ * You cannot use ft_memset (or memset) to initialize pthread_mutex_t
+ * because pthread_mutex_t is not just a block of memory. 
+ * It is a system-level object with an internal structure managed by the operating system or threading library. Simply zeroing out the memory (as ft_memset would do) is insufficient and could lead to undefined behavior when you try to use the mutex.
+ * @note
+ * Number of Forks = Number of Philosophers:
+ * (1) malloc 
+ * The table->forks array holds the mutexes for all the forks.
+ * Since the number of forks depends on the number of philosophers (philo_count), 
+ * its size can only be determined at runtime.
+ * 
+ * 
+ * - Fork mutexes (table->forks): 
+ * Each fork is represented by a mutex to ensure that only one philosopher can "pick up" a fork at a time. Without these mutexes, multiple threads could access the same resource simultaneously, leading to undefined behavior.
+ * - Print mutex (table->print_mutex): 
+ * Ensures that log messages (e.g., a philosopher eating or dying) are printed sequentially without overlapping from multiple threads.
+ * - Death mutex (table->death_mutex): 
+ * Protects access to shared data related to the simulation's termination conditions, like detecting a philosopher's death.
+ * 
+ * @note 
+ * - Philosopher 1 uses:
+ *   Left fork: forks[0]
+ *   Right fork: forks[1]
+ * - Philosopher 2 uses:
+ *   Left fork: forks[1]
+ *   Right fork: forks[0]
+ * 
+ * N Philosophers:
+ * N Forks:
+ * The array size equals nb_philo (e.g., for 5 philosophers, there are 5 forks).
+ * The fork for each philosopher is defined using:
+ * Left fork: forks[i]
+ * Right fork: forks[(i + 1) % nb_philo]
+ * This ensures the last philosopher's right fork wraps around to forks[0].
+ * 
+ */
+int init_mutex(t_table *table)
+{
+    int i;
 
-/** init philo */
+    i = -1;
+    //pthread_mutex_t *forks;
+    if ( table->nb_philo)
+    table->forks = malloc(sizeof(pthread_mutex_t) * table->nb_philo);
+    if (!table->forks)
+        return (FAILURE);
+    //allocate forks
+    while (++i < table->nb_philo)
+        pthread_mutex_init(&table->forks[i], NULL);
+    //init print mutex
+    if (pthread_mutex_init(&table->print_mutex, NULL))
+        return (FAILURE);
+    if (pthread_mutex_init(&table->death_mutex, NULL))
+        return (FAILURE);
+    return (SUCCESS);
+}
+
+/** INIT_PHILO
+ * @note
+ * ft_memset is not used here
+ * cuz initialization involves field-specific logic, complex values
+ * (e.g., pointers and get_time()), and mutex initialization.
+ * A loop with explicit assignments is more suitable for this case.
+ */
+int init_philo(t_table *table)
+{
+    int i;
+
+    i = -1;
+    table->philos = malloc(sizeof(t_philo) * (table->nb_philo));
+    if (!table->philos)
+        return (FAILURE);
+    //philo 1 | id: 1, fork: 1
+    while (++i < table->nb_philo)
+    {
+        table->philos[i].id = i + 1; //1 philo, 2 philo
+        table->philos[i].eat_count = 0; 
+        table->philos[i].last_meal_time = get_time(); 
+        table->philos[i].ph_table = table;
+        table->philos[i].left_fork = &table->forks[i]; //1 fork frm left
+        table->philos[i].right_fork = &table->forks[(i + 1) % (table->nb_philo)];
+    }
+    if (pthread_mutex_init(&table->philos[i].meal_mutex, NULL))
+        return (FAILURE);
+    return (SUCCESS);
+}
+
+/** START_FEAST 
+ * pthread_create() -> 2 philo 2 threads, etc
+ * 
+*/
 int start_feast(t_table *table)
 {
+
+pthread_create
+
     return (SUCCESS);
+}
+
+/**CLEANUP_ALL*/
+void    cleanup_all(t_table *table)
+{
+
 }
